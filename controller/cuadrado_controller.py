4231b0c487c2c4a3bc14ff8f7296905c2ae4bae1
Luis Noriega <lnoriegan3w@gmail.com>
2024-09-01 20:44:18 -0700

    fixing messages on modals

--- controller/cuadrado_controller.py
+++ controller/cuadrado_controller.py
@@ -11,10 +11,11 @@ def cuadrado_calcular():
     accion = request.form['accion']
     lado = float(request.form['lado'])
     cuadrado = Cuadrado(lado)
+    nombre_accion = "área" if accion == 'calcular_area' else "perímetro"
 
     if accion == 'calcular_area':
         resultado = cuadrado.calcular_area()
     elif accion == 'calcular_perimetro':
         resultado = cuadrado.calcular_perimetro()
         
-    return render_template('cuadrado_view.html', resultado=[accion, resultado])   
+    return render_template('cuadrado_view.html', resultado=[nombre_accion, resultado])   
